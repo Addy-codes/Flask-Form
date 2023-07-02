7ca70c27baf2dc913d7badcc20f5f4240b0d7abf
Addy-codes <adeeb9839@gmail.com>
2023-07-02 21:33:05 +0530

    final

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import mysql.connector
 
 app = Flask(__name__)
 
+# Establish a connection to the MySQL database
 connection = mysql.connector.connect(
     host="localhost",
     port="3307",
@@ -10,6 +11,8 @@ connection = mysql.connector.connect(
     user="root",
     password="qwerty123",
 )
+
+# Create a cursor object to execute MySQL queries
 cursor = connection.cursor()
 
 student = []
@@ -17,18 +20,21 @@ student = []
 
 @app.route("/")
 def index():
+    # Render the index.html template for the landing page
     return render_template("index.html")
 
 
 @app.route("/form", methods=["GET", "POST"])
 def form():
     if request.method == "POST":
+        # Retrieve form data submitted by the user
         name = request.form["name"]
         age = request.form["age"]
         college = request.form["college"]
         email = request.form["email"]
         major = request.form["major"]
 
+        # Insert the student data into the MySQL database
         cursor.execute(
             "INSERT INTO students(name, age, college, email, major) VALUES (%s,%s,%s,%s,%s)",
             (name, age, college, email, major),
@@ -38,21 +44,27 @@ def form():
         # Redirect to the submit page
         return redirect("/submit")
 
+    # Render the form.html template for the student form
     return render_template("form.html")
 
 
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
+    # Render the submit.html template for the submit page
     return render_template("submit.html")
 
 
 @app.route("/show")
 def show():
+    # Retrieve all student records from the MySQL database
     cursor.execute("SELECT * FROM students;")
     students = cursor.fetchall()
     print(students)
+
+    # Render the show.html template and pass the students data
     return render_template("show.html", students=students)
 
 
 if __name__ == "__main__":
+    # Run the Flask app in debug mode
     app.run(debug=True)
